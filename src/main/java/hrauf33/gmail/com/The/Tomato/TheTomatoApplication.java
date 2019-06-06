574b201c0ec31dee6b9654f7f486efb250d4ec43
Hana M Rauf <honey@hanas-air.home>
2019-06-06 16:48:46 +0100

    Initial commit

---
+++ src/main/java/hrauf33/gmail/com/The/Tomato/TheTomatoApplication.java
@@ -0,0 +1,13 @@
+package hrauf33.gmail.com.The.Tomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTomatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheTomatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hrauf33/gmail/com/The/Tomato/TheTomatoApplicationTests.java
@@ -0,0 +1,16 @@
+package hrauf33.gmail.com.The.Tomato;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheTomatoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
